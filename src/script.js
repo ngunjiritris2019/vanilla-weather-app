b564769364fc666fe1dab9a8661f5a8f276d83ce
ngunjiritris2019 <140059988+ngunjiritris2019@users.noreply.github.com>
2023-09-10 14:50:38 +0300

    console.log(response)
    To check if the function is working

--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
+function displayWeather(response) {
+  console.log(response);
+}
 let city = "Nairobi";
 let apiKey = "cf25ff98e4a3826d13328976b7728fc4";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayWeather);
