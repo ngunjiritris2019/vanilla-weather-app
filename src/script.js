6e435241dfbb7d160508f5ca51c6e67ae61c08c8
ngunjiritris2019 <140059988+ngunjiritris2019@users.noreply.github.com>
2023-09-10 18:36:42 +0300

    Add weather icons from OpenWeatherApp

--- src/script.js
+++ src/script.js
@@ -1,7 +1,74 @@
-function displayWeather(response) {
-  console.log(response);
+function currentTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}: ${minutes}`;
 }
-let city = "Nairobi";
-let apiKey = "cf25ff98e4a3826d13328976b7728fc4";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayWeather);
+function displayWeather(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let maximumTempElement = document.querySelector("#max-temperature");
+  maximumTempElement.innerHTML = Math.round(response.data.main.temp_max);
+
+  let minimumTempElement = document.querySelector("#min-temperature");
+  minimumTempElement.innerHTML = Math.round(response.data.main.temp_min);
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let humidElement = document.querySelector("#humidity");
+  humidElement.innerHTML = Math.round(response.data.main.humidity);
+
+  let feelsLikeElement = document.querySelector("#feels-like");
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+
+  let timeElement = document.querySelector("#current-time");
+  timeElement.innerHTML = currentTime(response.data.dt * 1000);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  icon.setAttribute("alt", response.data.weather[0].description);
+}
+function search(city) {
+  let apiKey = "cf25ff98e4a3826d13328976b7728fc4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#input-city");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Nairobi");
